6ca404361846958dd049c6bbc3e4c9aad3a03981
rdrachenberg <31778670+rdrachenberg@users.noreply.github.com>
2018-01-09 18:48:18 -0500

    Starting Files

---
+++ assets/javascript/gotCoinJS.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC_iqQW_F3errT4mRYM2IN0KDZAhx3U-hg",
+    authDomain: "gotcoin-db44a.firebaseapp.com",
+    databaseURL: "https://gotcoin-db44a.firebaseio.com",
+    projectId: "gotcoin-db44a",
+    storageBucket: "",
+    messagingSenderId: "492118808139"
+  };
+  firebase.initializeApp(config);
