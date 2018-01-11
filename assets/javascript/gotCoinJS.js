08f2550c94a67478004926d1a8721f5946f2ba7d
Brad Lawson <bradlawson@cfl.rr.com>
2018-01-11 18:29:11 -0500

    updated with modals and style

--- assets/javascript/gotCoinJS.js
+++ assets/javascript/gotCoinJS.js
@@ -8,3 +8,29 @@
     messagingSenderId: "492118808139"
   };
   firebase.initializeApp(config);
+
+  // Get the modal
+var modal = $('#myModal');
+
+// Get the button that opens the modal
+var link = $("#rscLink");
+
+// Get the <span> element that closes the modal
+var span = $('.close');
+
+// When the user clicks on the button, open the modal 
+$('#rscLink').on("click", function() {
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
