3811d141c4a08f992beeb4cf7684529eea0a19b4
unknown <jkue3000@gmail.com>
2018-01-11 20:43:16 -0500

    Need to get INTRADAY time to display 1min price info from API

--- assets/javascript/alphavantageAPI.js
+++ assets/javascript/alphavantageAPI.js
@@ -1,6 +1,9 @@
 
+// create an array that holds stocks to display in the six stock box cards automatically.
+var presetStockArray = ["AAPL", "DST", "SSNC", "GOOGL", "AMZN", "TSLA"];
 
 
+// if user searches then remove the #5 in the array and add user input into #0 in the array
 
 
 
@@ -9,7 +12,7 @@ $(".stockSearch").on('click', function(){
 	// API url to get stock info
 	var alphavantageApiKey = "&apikey=3ZIHGQKVNFF4IYF5";
 	var userStockSearch = $(".form-control").val().trim();
-	var alphavantageURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+userStockSearch+alphavantageApiKey;
+	var alphavantageURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + userStockSearch + "&interval=1min" + alphavantageApiKey;
 
 	$.ajax({
         url: alphavantageURL,
@@ -20,23 +23,41 @@ $(".stockSearch").on('click', function(){
 	.done(function(response) {
 
 		console.log(response);
-
+		// gettin user input
 		var results = response;
 
-		var metaData = results["Time Series (Daily)"];
-		var date = metaData["2018-01-09"];
+		// getting the current date to show daily stock information
+		var currentTime = moment().format();
+		var dateFormat = moment(currentTime).format("YYYY-MM-DD HH:MM:00"); 
+		console.log("CURRENT TIME: " + moment(currentTime).format("YYYY-MM-DD HH:MM:00"));
 
+		// going inside the API and getting the information to display in the cardboxes 
+		var timeSeriesDaily = results["Time Series (1min)"];
+		var date = timeSeriesDaily[dateFormat];
+
+		console.log(timeSeriesDaily);
+		console.log(date);
+
+		// get the SYMBOL information to display in the cardbox as the boxcard header
+		var metaData = results["Meta Data"];
+		var stockSymbol = metaData["2. Symbol"];
+		console.log(stockSymbol);
+
+    	// storing the date into stockInformation variable so I can dsiplay the stockInformation to the HTML cards 
+		var stockInformation = date;
+		console.log(stockInformation);
+
+		// making newDiv dynamically to show the stockSymbolto the html card box
 		
-'Figure out how to convert current date to the date format LIKE above^^
-		var currentDate = moment().format(); 
-		
+
+		for (i = 0; i < presetStockArray.length; i++){
+			
+		}
 
 
-		var volume = date["5. volume"];
-
-		console.log(volume);
-
+		// $('.card-body').text(newDiv);
 	
 	});
 	
 });
+
