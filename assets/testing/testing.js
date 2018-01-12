56c7fce2f83ac79b8c497f84da251e6febccb2dd
rdrachenberg <31778670+rdrachenberg@users.noreply.github.com>
2018-01-12 00:32:16 -0500

    corrected files, CoinBase API call working

--- assets/testing/testing.js
+++ assets/testing/testing.js
@@ -1,14 +1,3 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC_iqQW_F3errT4mRYM2IN0KDZAhx3U-hg",
-    authDomain: "gotcoin-db44a.firebaseapp.com",
-    databaseURL: "https://gotcoin-db44a.firebaseio.com",
-    projectId: "gotcoin-db44a",
-    storageBucket: "",
-    messagingSenderId: "492118808139"
-  };
-  firebase.initializeApp(config);
-
 
 // npm install crypto
 var crypto = require('crypto');
@@ -53,4 +42,3 @@ request(options,function(err, response){
     if (err) console.log(err);
     console.log(response.body);
 });
-
